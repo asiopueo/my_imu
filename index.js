1fc42bb45d4688fb180d487932035be170eb1341
Martin <martin.lippl@pueo.de>
2018-07-07 16:51:47 +0200

    first init

---
+++ index.js
@@ -0,0 +1,32 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+var serialport = require('serialport');
+var port = new serialport('/dev/ttyACM0',
+	{
+		baudRate: 115200,
+	});
+
+
+/*app.get('/', function(req, res){
+  res.send('<h1>Hello world</h1>');
+});*/
+
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html');
+});
+    
+io.on('connection', function(socket){
+	console.log('a user connected');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+port.on('data', function(data){
+	console.log('Data: ', data.toString('utf8'));
+});
