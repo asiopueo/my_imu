0a987352b73d305476204340f3632219c3397e31
Martin <martin.lippl@pueo.de>
2018-07-14 15:12:43 +0200

    added advanced lighting and shadow casting

--- client/client.js
+++ client/client.js
@@ -62,7 +62,6 @@ function socketClient() {
 
 
 function init() {
-
     container = document.createElement('div');
     document.body.appendChild(container);
 
@@ -79,36 +78,77 @@ function init() {
 
     // Set up camera
     camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000);
-    camera.position.y = 150;
+    camera.position.y = 350;
     camera.position.z = 500;
+    camera.lookAt(new THREE.Vector3(0, 150, 0));
 
     scene = new THREE.Scene();
 
     // Create cube
     var geometry = new THREE.BoxGeometry( 200, 50, 200 );
-
     for ( var i = 0; i < geometry.faces.length; i += 2 ) {
         var hex = Math.random() * 0xffffff;
         geometry.faces[ i ].color.setHex( hex );
         geometry.faces[ i + 1 ].color.setHex( hex );
     }
-
-    var material = new THREE.MeshBasicMaterial( { vertexColors: THREE.FaceColors, overdraw: 0.5 } );
+    //var material = new THREE.MeshBasicMaterial( { vertexColors: THREE.FaceColors, overdraw: 0.5 } );
+    var material = new THREE.MeshPhongMaterial({overdraw: true, color: 0xff0000});
     cube = new THREE.Mesh( geometry, material );
-    cube.position.y = 150;
+    cube.position.y = 200;
+    cube.castShadow = true;
     scene.add(cube);
 
+
+
     // Create background plane
-    var geometry = new THREE.PlaneBufferGeometry( 400, 200 );
+    var geometry = new THREE.PlaneBufferGeometry( 800, 400 );
     geometry.applyMatrix( new THREE.Matrix4().makeRotationX( - Math.PI / 2 ) );
-    var material = new THREE.MeshBasicMaterial( { color: 0xe0e0e0, overdraw: 0.5 } );
+    //var material = new THREE.MeshBasicMaterial( { color: 0xe0e0e0, overdraw: 0.5 } );
+    var material = new THREE.MeshPhongMaterial( { color: 0xe0e0e0, overdraw: 0.5 } );
     plane = new THREE.Mesh( geometry, material );
+    plane.receiveShadow = true;
     scene.add(plane);
+
+
+    
+    // Add hemisphere light
+    hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.6 );
+    //hemiLight.color.setHSL( 0.6, 1, 0.6 );
+    hemiLight.color.setHSL( 1, 1, 1 );
+    hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
+    hemiLight.position.set( 0, 500, 0 );
+    scene.add( hemiLight );
+
+    // Add ambient light
+    //var ambient = new THREE.AmbientLight(0x404040);
+    //scene.add(ambient);
+
+    // Add directional light
+    dirLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+    dirLight.color.setHSL( 0.1, 1, 0.95 );
+    //dirLight.position.set( -1, 1.75, 1 );
+    dirLight.position.set( -2, 5, 1 );
+    dirLight.position.multiplyScalar( 50 );
+
+    dirLight.castShadow = true;
+    dirLight.shadow.mapSize.width = 4096;
+    dirLight.shadow.mapSize.height = 4096;
+    var d = 1000;
+    dirLight.shadow.camera.left = -d;
+    dirLight.shadow.camera.right = d;
+    dirLight.shadow.camera.top = d;
+    dirLight.shadow.camera.bottom = -d;
+    dirLight.shadow.camera.far = 3500;
+    dirLight.shadow.bias = -0.0001;
+    scene.add( dirLight );
+
 
 
     renderer = new THREE.WebGLRenderer();
     renderer.setClearColor(0xf0f0f0);
     renderer.setSize( window.innerWidth, window.innerHeight);
+    // Shadow casting and receiving:
+    renderer.shadowMap.enabled = true;
     container.appendChild(renderer.domElement);
 
     window.addEventListener('resize', onWindowResize, false);
